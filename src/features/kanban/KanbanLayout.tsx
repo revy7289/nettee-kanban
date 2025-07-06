42e359c2b90b320d4c6089d0ee7766d5085d79e6
revy7289 <revy7289@gmail.com>
2025-07-06 21:49:04 +0900

    feat: dnd 오프셋 설정 원복

--- src/features/kanban/KanbanLayout.tsx
+++ src/features/kanban/KanbanLayout.tsx
@@ -375,13 +375,13 @@ export function KanbanLayout() {
   };
 
   const getNearestIndicator = (e: DragEvent, indicators: HTMLElement[]) => {
-    const DISTANCE_OFFSET = 100;
+    const DISTANCE_OFFSET = 50;
 
     const el = indicators.reduce(
       (closest, child) => {
         const box = child.getBoundingClientRect();
 
-        const offset = e.pageY - (box.top + DISTANCE_OFFSET);
+        const offset = e.clientY - (box.top + DISTANCE_OFFSET);
 
         if (offset < 0 && offset > closest.offset) {
           return { offset: offset, element: child };
