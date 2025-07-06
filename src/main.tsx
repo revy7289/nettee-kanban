c11a254ea38c228ad92be3a89c91eff76ca3e196
revy7289 <revy7289@gmail.com>
2025-07-06 21:55:05 +0900

    fix: 메인>앱 import 명시적으로 지정(버셀배포용)

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import '@/shared/styles/global.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { App } from '@/App';
+import { App } from '@/App/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
